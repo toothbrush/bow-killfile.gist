4e6431b305291cbd7ef9402395a7ccd0d47e6415
paul <423357+toothbrush@users.noreply.github.com>
2023-03-21 15:11:57 +1100

    formatting

--- BOW-killfile.user.js
+++ BOW-killfile.user.js
@@ -98,15 +98,15 @@ function getElementByXpath(path) {
 
 // I dunno, maybe this is more portable. Hm.
 function GM_addStyle(css) {
-  const style = document.getElementById("GM_addStyleBy8626") || (function() {
-    const style = document.createElement('style');
-    style.type = 'text/css';
-    style.id = "GM_addStyleBy8626";
-    document.head.appendChild(style);
-    return style;
-  })();
-  const sheet = style.sheet;
-  sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
+    const style = document.getElementById("GM_addStyleBy8626") || (function() {
+        const style = document.createElement('style');
+        style.type = 'text/css';
+        style.id = "GM_addStyleBy8626";
+        document.head.appendChild(style);
+        return style;
+    })();
+    const sheet = style.sheet;
+    sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
 }
 
 GM_addStyle(`.wrapper {

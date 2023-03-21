e0b046acfae87977f0e7dd4fd5da41f33cf1c4b4
paul <423357+toothbrush@users.noreply.github.com>
2023-03-21 15:44:08 +1100

    cloudy

--- BOW-killfile.user.js
+++ BOW-killfile.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://gist.github.com/toothbrush/364c15ec7192e60ffd94576773c4b99c
 // @updateURL    https://gist.githubusercontent.com/toothbrush/364c15ec7192e60ffd94576773c4b99c/raw/BOW-killfile.user.js
 // @downloadURL  https://gist.githubusercontent.com/toothbrush/364c15ec7192e60ffd94576773c4b99c/raw/BOW-killfile.user.js
-// @version      0.52
+// @version      0.53
 // @description  block trolls
 // @author       toothbrush
 // @match        https://news.ycombinator.com/item*
@@ -209,6 +209,7 @@ function mutationHandler (mutationRecords) {
         [/\bHN\b/g, 'BOW'],
         [/a couple(?! of)/g, 'a couple of'],
         [/\bcloud\b/g, "other people's computer"],
+        [/\bCloud\b/g, "Other People's Computer"],
         [/\bGPT\b/g, 'Magic'],
         [/\bAI\b/g, 'MAGIC'],
         [/\bOpenAI\b/gi, 'Open Art Thieves'],
